808e84e6a475412972473cadc50bca0323725365
“Max” <louxiaomax@gmail.com>
2023-04-05 20:47:48 -0700

    test_file
    2nd sub

--- app/test_file.py
+++ app/test_file.py
@@ -21,8 +21,9 @@ def main():
     # #test case1 -- put
     file_name = "test"
     data = '{"Max": "daddy"}'
+    command = ""
     correct("put", put_test(file_name, data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case2 -- post
     file_name = "test"
@@ -38,41 +39,41 @@ def main():
     file_name = "test/subtest"
     data = '{"yxj":23331}'
     correct("patch", patch_test(file_name, data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case5 -- patch 3 (key exist )
     file_name = "test/subtest/ssubtest"
     data = '{"lcc":123455}'
     correct("patch", patch_test(file_name, data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case6 -- delete ssubtest
     file_name = "test/subtest/ssubtest"
     correct("delete", delete_test(file_name))
-    correct("get", get_test("test/subtest"))
+    correct("get", get_test("test/subtest",command))
 
     #test case7 -- put a list in subtest
     file_name = "test/subtest"
     data = '{"school":["usc","ucla","stanford"]}'
     correct("put", put_test(file_name,data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case8 -- put a list in usc
     file_name = "test/subtest/school/0"
     data = '{"color":["blue","red","yellow"]}'
     correct("put", put_test(file_name, data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case9 -- delete one element in usc
     file_name = "test/subtest/school/0/color/1"
     correct("delete", delete_test(file_name))
-    correct("get", get_test("test/subtest/school/0/color"))
+    correct("get", get_test("test/subtest/school/0/color", command))
 
     #test case10 -- patch yellow to a list of three colors
     file_name = "test/subtest/school/0/color/2"
     data = '{"yellow": ["light y","dark y"]}'
     correct("patch", patch_test(file_name, data))
-    correct("get", get_test("test/subtest/school/0/color"))
+    correct("get", get_test("test/subtest/school/0/color", command))
 
     #test case11 -- post new color in yellow
     file_name = "test/subtest/school/0/color/2"
@@ -82,28 +83,27 @@ def main():
     #test case12 -- delete the whole usc
     file_name = "test/subtest/school/0"
     correct("delete", delete_test(file_name))
-    correct("get", get_test("test/subtest/school"))
+    correct("get", get_test("test/subtest/school",command))
 
     #test case13 -- add elements in usc by patch
     file_name = "test/subtest/school/usc"
     data = '{"students":[{"name":"yxj", "id":12},{"name":"lx", "major":"ads", "hobbies":["cook",{"sports":["esport","basketball"]}]}]}'
     correct("patch", patch_test(file_name, data))
-    correct("get", get_test(file_name))
+    correct("get", get_test(file_name,command))
 
     #test case14 -- delete hobbies
     file_name = "test/subtest/school/usc/students/1/hobbies"
     correct("delete", delete_test(file_name))
-    correct("get", get_test("test/subtest/school/usc/students/1"))
+    correct("get", get_test("test/subtest/school/usc/students/1", command))
 
     #test case15 -- tried path that does not exist, nothing change
     file_name = "test/subtest/school/usc/students/1/hobbies"
     correct("delete", delete_test(file_name))
-    correct("get", get_test("test/subtest/school/usc/students/1"))
+    correct("get", get_test("test/subtest/school/usc/students/1", command))
 
     # test case15 -- delete with wrong path
     file_name = "test/subtest/school2/usc/students/"
     correct("delete", delete_test(file_name))
-
 
 
 
@@ -142,13 +142,13 @@ def main():
     #correct("delete", delete_test(file_name)) -- in mongo it is good, but not work in firebase
 
     # test case 23 -- get with invalid path  -- it works
-    correct("get", get_test("test/.w"))
+    correct("get", get_test("test/.w", command))
 
     # test case 24 -- get with right path
-    correct("get", get_test("test/subtest/school"))
+    correct("get", get_test("test/subtest/school", command))
 
     #test case 25 -- get with wrong path
-    #correct("get", get_test("test/subtest/school8"))
+    correct("get", get_test("test/subtest/school8", command))
 
     #look into details
     url1 = "http://127.0.0.1:5000/" + "test/subtest/school8" + ".json"
@@ -156,18 +156,136 @@ def main():
     #res1 = json.loads(res1.text)
     print("response from mongo is: " + str(res1))
 
+    #------- ORDERBY/ limitToFirst/ limitToLast/ equalTo/ startAt/ endAt ----
+    print("-------------------Test ORDERBY/ limitToFirst/ limitToLast/ equalTo/ startAt/ endAt")
+    print()
+    print()
+    file_name = "test/ob"
+    name = '["iphone","samsung","vivio","oppo","xiaomi"]'
+    correct("put", put_test(file_name, name))
+
+    name = ["iphone","samsung","vivio","oppo","xiaomi"]
+    quantity =  [10, 60, 44, 20, 100]
+    price = [9999, 8888, 3333, 4422, 3888]
+
+    file_name = "test/ob/0"
+    data1 = '{"name":"iphone" , "quantity": 10, "price":9999}'
+    data2 = '{"name":"samsung" , "quantity": 60, "price":8888}'
+    data3 = '{"name":"vivo" , "quantity": 44, "price":3333}'
+    data4 = '{"name":"oppo" , "quantity": 20, "price":4422}'
+    data5 = '{"name":"xiaomi" , "quantity": 100, "price":3888}'
+
+    correct("patch", patch_test("test/ob/0",data1))
+    correct("patch", patch_test("test/ob/1", data2))
+    correct("patch", patch_test("test/ob/2", data3))
+    correct("patch", patch_test("test/ob/3", data4))
+    correct("patch", patch_test("test/ob/4", data5))
+
+    #OrderBy1 -- on price
+    #e.g.     ‘….user.json?orderBy="$key"&startAt="102"&print=pretty’
+    file_name = "test/ob"
+    command ='?orderBy="price"&limitToLast=1'
+    correct("orderBy", get_test(file_name, command)) #should be iphone
+
+    #OrderBy2 -- on name
+    command = '?orderBy="name"&limitToFirst=1'
+    correct("orderBy", get_test(file_name, command)) #should be iphone
+
+    # # OrderBy2 -- an invalid key -- error
+    # command = '?orderBy="na"&limitToFirst=1'
+    # correct("orderBy", get_test(file_name, command))  # should be iphone
+
+    #startAt
+    command = '?orderBy="name"&startAt="x"&endAt="z"'
+    correct("startAt", get_test(file_name, command))  # should be xiaomi,vivi
+
+    #startAt
+    command = '?orderBy="price"&startAt=7000'
+    correct("startAt", get_test(file_name, command))  # should be samsung,iphone
+
+    #endtAt
+    command = '?orderBy="price"&startAt=7000&endAt=9000'
+    correct("endAt", get_test(file_name, command))  # should be samsung
+
+    #startAt
+    command = '?orderBy="price"&startAt=10000&endAt=9000'
+    correct("startAt", get_test(file_name, command))  # should be null
+
+    # startAt
+    command = '?orderBy="price"&startAt=10000&endAt=13000'
+    correct("startAt", get_test(file_name, command))  # should be null
+
+    # startAt
+    command = '?orderBy="price"&startAt=-10'
+    correct("startAt", get_test(file_name, command))  # should be null
+
+    # endAt
+    command = '?orderBy="price"&startAt=-10&endAt=18.8'
+    correct("endAt", get_test(file_name, command))  # should be null
+
+
+    #equalTo
+    command = '?orderBy="price"&equalTo=8888'
+    correct("startAt", get_test(file_name, command))  # should be samsung
+
+    # equalTo
+    command = '?orderBy="quantity"&equalTo=20'
+    correct("equalTo", get_test(file_name, command))  # should be samsung
+
+    #equalTo
+    command = '?orderBy="name"&equalTo="vivo"'
+    correct("equalTo", get_test(file_name, command))  # should be vivo
+
+    #equalTo
+    command = '?orderBy="name"&equalTo="vivwwwo"'
+    correct("equalTo", get_test(file_name, command))  # should be null
+
+    #limitToFirst
+    command = '?orderBy="name"&limitToFirst=2'
+    correct("limitToFirst", get_test(file_name, command))  # should be
+
+    # limitToFirst
+    command = '?orderBy="name"&limitToFirst=-1'
+    correct("limitToFirst", get_test(file_name, command))  # should be
+
+    # limitToFirst
+    command = '?orderBy="quantity"&limitToFirst=1'
+    correct("limitToFirst", get_test(file_name, command))  # should be
+
+    # limitToFirst
+    command = '?orderBy="quantity"&limitToFirst=20'
+    correct("limitToFirst", get_test(file_name, command))  # should be
+
+    # limitToLast
+    command = '?orderBy="name"&limitToLast=2'
+    correct("limitToLast", get_test(file_name, command))  # should be
+
+    # limitToLast
+    command = '?orderBy="quantity"&limitToLast=1'
+    correct("limitToLast", get_test(file_name, command))  # should be
+
+    # limitToLast
+    command = '?orderBy="quantity"&limitToLast=-1'
+    correct("limitToLast", get_test(file_name, command))  # should be error
+
+    # limitToLast
+    command = '?orderBy="quantity"&limitToLast=20'
+    correct("limitToLast", get_test(file_name, command))  # should be error
+
+
     #clean()
 
 
 
 
-def get_test(file_name):
-    url1 = "http://127.0.0.1:5000/" + file_name + ".json"
+
+def get_test(file_name, command =None):
+    url1 = "http://127.0.0.1:5000/" + file_name + ".json" + command
     res1 = requests.get(url1, headers=headers)
     res1 = json.loads(res1.text)
     print("response from mongo is: " + str(res1))
 
-    url2 = "https://cloudcomputing1-f0a9c-default-rtdb.firebaseio.com/" + file_name + ".json"
+    url2 = "https://cloudcomputing1-f0a9c-default-rtdb.firebaseio.com/" + file_name + ".json" + command
     res2 = requests.get(url2)
     res2 = json.loads(res2.text)
     res2 = input_data_transform(res2)
